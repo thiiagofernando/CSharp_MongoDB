788ea412ffcc4d83ddcb58d20f83b087b30e5cb5
Thiago Fernando <thiago.fernando@msn.com>
2018-05-01 22:15:41 -0400

    Primeiro commit

---
+++ CSharp_MongoDB/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+
+namespace CSharp_MongoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var client = new MongoClient("mongodb://localhost:27017");
+                var database = client.GetDatabase("loja");
+                var colecao = database.GetCollection<Usuarios>("usuarios");
+
+                var usuario = new List<Usuarios>
+                {
+                    new Usuarios
+                    {
+                      Login = "thiago2",
+                      Senha = "1234567"
+                    },
+                    new Usuarios
+                    {
+                      Login = "thiago2",
+                      Senha = "1234568"
+                    }
+                };
+                colecao.InsertMany(usuario);
+                Console.WriteLine("Dados Inseridos com Sucesso.");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro: {e.Message}");
+            }
+        }
+    }
+}
